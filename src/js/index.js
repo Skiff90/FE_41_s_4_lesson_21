2668505c4f4f1ddcf637e50e97d0417357e7a6b9
Vladislav Keller <skiffvlad90@gmail.com>
2024-06-21 18:52:21 +0300

    Revert "1.1"
    This reverts commit df7605d132f6799b81600e00aa52511fc1ed4303.

--- src/js/index.js
+++ src/js/index.js
@@ -36,7 +36,7 @@ async function createPost(title, content) {
 
 async function upgradePost(id, title, content) {
   try {
-    const response = await axios.put(`http://localhost:3000/posts/${id}`, {
+    const response = await axios.post(`http://localhost:3000/posts/{id}`, {
       title,
       content,
     });
@@ -48,7 +48,7 @@ async function upgradePost(id, title, content) {
 
 async function deletePost(id) {
   try {
-    const response = await axios.delete(`http://localhost:3000/posts/${id}`);
+    const response = await axios.post(`http://localhost:3000/posts/{id}`);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -74,39 +74,3 @@ function renderPosts(posts) {
   const html = template(context);
   document.getElementById("postsContainer").innerHTML = html;
 }
-
-document.addEventListener("click", async (event) => {
-  if (event.target.classList.contains("editPostButton")) {
-    const id = event.target.dataset.id;
-    const post = posts.find((post) => post.id == id);
-    const title = promt("Редагувати заголовок", post.title);
-    const content = promt("Редагувати зміст", post.content);
-    await upgradePost(id, title, content);
-    const posts = await fetchPosts();
-    renderPosts(posts);
-  }
-  if (event.target.classList.contains("deletePostButton")) {
-    const id = event.target.dataset.id;
-    await deletePost(id);
-    const posts = await fetchPosts();
-    renderPosts(posts);
-  }
-});
-
-document.addEventListener("submit", async (event) => {
-  if (event.target.classList.contains("createCommentForm")) {
-    event.preventDefault();
-    const postId = event.target.closest(".post").dataset.id;
-    const comment = event.target.querySelector(".commentInput").value;
-    await createComment(postId, comment);
-    const posts = await fetchPosts();
-    renderPosts(posts);
-  }
-});
-
-async function startApp() {
-  const posts = await fetchPosts();
-  renderPosts(posts);
-}
-
-startApp();
